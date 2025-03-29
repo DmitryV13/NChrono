f98a88d1a47f4dae0a5671ba9b839f5344352233
Kwameldx666 <Wonderful_by@mail.ru>
2025-03-29 09:11:29 +0200

    merge mail page with all filtres

--- FeaneMVC/Migrations/20250329011051_migr.Designer.cs
+++ FeaneMVC/Migrations/20250329011051_migr.Designer.cs
@@ -4,6 +4,7 @@ using FinalProject.DbModel;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace FeaneMVC.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250329011051_migr")]
+    partial class migr
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
